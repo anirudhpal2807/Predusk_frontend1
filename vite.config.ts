0c43b786a512853f44c47d248ed06ca4257fe067
anirudhpal2807 <anirudhpal2807@gmail.com>
2025-08-31 13:39:28 +0530

    optimise backend

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    sourcemap: false
-  },
   server: {
     port: 3000,
     host: true,
@@ -18,9 +13,5 @@ export default defineConfig({
         secure: false,
       }
     }
-  },
-  preview: {
-    port: 3000,
-    host: true
   }
 })
